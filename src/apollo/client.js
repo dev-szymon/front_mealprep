9963773d1352f6604acc2922fd3bac759b871bfc
Szymon <szymonphotoshop@gmail.com>
2020-05-07 16:50:36 +0200

    typo

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,6 +1,6 @@
 import ApolloClient from "apollo-boost"
 import fetch from "isomorphic-fetch"
-const api = process.env.API_URI || require("../../variables.js")
+const api = process.env.API_URI
 
 export const client = new ApolloClient({
   uri: api,
