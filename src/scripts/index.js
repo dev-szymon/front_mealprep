8027cd2582b98197b04af2454ba0cae9acc5e8b2
Szymon <szymonphotoshop@gmail.com>
2020-07-07 10:03:03 +0200

    added cloudinary config to build

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,13 @@
 require("dotenv").config()
 const fs = require("fs")
 fs.writeFileSync(
-  "./.test",
+  "./.env",
   `GATSBY_GRAPHQL_API=${process.env.GATSBY_GRAPHQL_API}\nGATSBY_UPLOAD_URL=${process.env.GATSBY_GRAPHQL_API}\n`
+)
+fs.writeFileSync(
+  "./src/config/index.js",
+  `export default {
+  cloud_name: "${process.env.GATSBY_CLOUD_NAME}",
+  upload_preset: "${process.env.GATSBY_UPLOAD_PRESET}",
+}`
 )
