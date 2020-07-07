83555a25f0a06a2530173e1ba72bbc877b120926
Szymon <szymonphotoshop@gmail.com>
2020-07-07 11:11:27 +0200

    added env

--- src/pages/cookbook/index.js
+++ src/pages/cookbook/index.js
@@ -1,10 +1,8 @@
 import React from "react"
 import Layout from "../../components/layout/layout"
 import { Link } from "gatsby"
-import { getAccessToken } from "../../auth"
 
 const Cookbook = () => {
-  console.log(getAccessToken())
   return (
     <Layout>
       <div>Cookbook</div>
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
 const fs = require("fs")
 fs.writeFileSync(
   "./.env",
-  `GATSBY_GRAPHQL_API=${process.env.GATSBY_GRAPHQL_API}\nGATSBY_UPLOAD_PRESET=${process.env.GATSBY_UPLOAD_PRESET}\nGATSBY_CLOUD_NAME=${process.env.GATSBY_CLOUD_NAME}\n`
+  `CLOUDINARY_URL=${process.env.CLOUDINARY_URL}\nGATSBY_GRAPHQL_API=${process.env.GATSBY_GRAPHQL_API}\nGATSBY_UPLOAD_PRESET=${process.env.GATSBY_UPLOAD_PRESET}\nGATSBY_CLOUD_NAME=${process.env.GATSBY_CLOUD_NAME}\n`
 )
